8d99a54353170881e7b8311ee0f3910e73a462ab
ItsHariKrishna <itsharikrishna8@gmail.com>
2022-04-24 15:45:37 +0530

    Update main.py
    - added user input so that the user can search for the top 100 albums for his/her preferred date
    - collected names of whole albums

--- Project 8/main.py
+++ Project 8/main.py
@@ -1,13 +1,20 @@
 from bs4 import BeautifulSoup
 import requests
 
-URL = "https://www.billboard.com/charts/hot-100/2000-08-12/"
 
-# year = input("To which year do you want to travel to? Type the date in this format (YYYY-MM-DD): ")
+year = input("To which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+URL = f"https://www.billboard.com/charts/hot-100/{year}"
+
 
 response = requests.get(URL)
 response.raise_for_status()
 data = response.text
 
 soup = BeautifulSoup(data, "html.parser")
-print(soup.prettify())
+
+# titles = soup.select(selector="div li h3.a-no-trucate")
+titles = soup.select(selector="div li h3.a-no-trucate")
+song_titles = [i.getText().strip("\n\t") for i in titles]
+print(song_titles)
+# song_titles = [title_name.text for title_name in titles]
+# print(song_titles)
